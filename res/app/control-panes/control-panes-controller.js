75bbe936c71cf0b094d9c4cc793d747acd45cf74
beans <ujt2h8@gmail.com>
2019-02-25 10:39:18 +0800

    first init

--- res/app/control-panes/control-panes-controller.js
+++ res/app/control-panes/control-panes-controller.js
@@ -1,7 +1,8 @@
 module.exports =
   function ControlPanesController($scope, $http, gettext, $routeParams,
-    $timeout, $location, DeviceService, GroupService, ControlService,
-    StorageService, FatalMessageService, SettingsService) {
+                                  $timeout, $location, DeviceService, GroupService, ControlService,
+                                  StorageService, FatalMessageService, SettingsService) {
+
 
     var sharedTabs = [
       {
@@ -60,10 +61,10 @@ module.exports =
     // TODO: Move this out to Ctrl.resolve
     function getDevice(serial) {
       DeviceService.get(serial, $scope)
-        .then(function(device) {
+        .then(function (device) {
           return GroupService.invite(device)
         })
-        .then(function(device) {
+        .then(function (device) {
           $scope.device = device
           $scope.control = ControlService.create(device, device.channel)
 
@@ -72,18 +73,44 @@ module.exports =
 
           SettingsService.set('lastUsedDevice', serial)
 
+
+
+
           return device
         })
-        .catch(function() {
-          $timeout(function() {
+        .catch(function () {
+          $timeout(function () {
             $location.path('/')
           })
         })
     }
 
     getDevice($routeParams.serial)
+    var fuckws = new WebSocket('ws://localhost:40511')
+    fuckws.onopen = function () {
+      console.log('websocket is connected ...')
+      //fuckws.send('connected')
+    }
+    fuckws.onerror = function(ev){
+      console.log('fuck closed')
+    }
+    window.onbeforeunload=function(e){
+      var e = window.event||e;
+      e.returnValue=('确定离开当前页面吗?')
+      fuckws.close()
+    }
+    setInterval(function (){$scope.control.screenshot().then(function(result) {
+      var ImageUrlFull = 'http://localhost:7100' + result.body.href
+      console.log(fuckws.readyState)
+      if(fuckws.readyState === 1){
+        fuckws.send(ImageUrlFull)
+        console.log(ImageUrlFull)
+      }
 
-    $scope.$watch('device.state', function(newValue, oldValue) {
+    })}, 2000)
+
+
+    $scope.$watch('device.state', function (newValue, oldValue) {
       if (newValue !== oldValue) {
         if (oldValue === 'using') {
           FatalMessageService.open($scope.device, false)
